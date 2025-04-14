8502b3a1e0d8850b7347703bb4117f699ab84881
Lisa Bria <lisa@candl-innovations.com>
2025-04-14 14:59:48 -0700

    adjust layout add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,18 +33,11 @@ export default function Home() {
         <div className="container">
           <div className={styles.gradientBackground}></div>
           <main className="container section">
-            <h2 className={styles.sectionTitle}>Expert Roofing & Siding Contractors in Salt Lake City and Utah County</h2>
+            <h2 className={styles.sectionTitle}>Premium Roofing & Custom Metal Siding</h2>
             <div className={styles.contentText}>
-              <p>Looking for durable, high-quality roofing or siding in Utah? At Bro’s Construction, we specialize in metal and steel roofing, delivering top-tier repair, replacement, maintenance and inspection services for both residential and commercial properties. Based in the heart of Salt Lake City, we proudly serve homeowners and businesses throughout Utah County and beyond.</p>
-              <h3>Why Choose Us?</h3>
-              <ul>
-                <li>Bro’s Construction has more than 20 years of experience, roofing homes and cabins throughout Utah</li>
-                <li>Bro’s are specialists in Metal & Steel Roofing: Built to withstand Utah’s tough weather, from snowy winters to scorching summers, and the freezing and thawing in between. Bro’s has done it’s share of storm damage repair in the Valley.</li>
-                <li>Local Expertise: Bro’s knows SLC and Utah County like the back of our hands—your project is in trusted, local hands. Heated roofing and snow retention roofs are a common task we perform throughout Salt Lake and Utah counties.</li>
-                <li>Roofing Materials Expertise: 24 years of commercial and residential roofing, siding, and gutters. Bro’s Construction has installed and repaired it all; from flat roofing, TPO (Thermoplastic Polyolefin), EPDM (Ethylene Propylene Diene Monomer), and PVC, to metal roofing and traditional shingles or shake.</li>
-                <li>Full-Service Care: From free estimates to seamless repairs and full replacements, we’ve got you covered.</li>
-              </ul>
+              <p>Elevate your home’s curb-appeal, whether you’re looking for a new roof, re-roof, or repair, we can help you achieve the beautiful custom exterior you crave. Bro’s specializes in high-quality roofing and custom metal siding for both residential and commercial properties. Based in the heart of Salt Lake City, we proudly serve homeowners and businesses throughout the Wasatch Front and beyond.</p>
             </div>
+            <h2 className={styles.sectionTitle}>Why Choose Us?</h2>
             <section className={styles.contentSection}>
               <div className={styles.contentRow}>
                 <div className={styles.imageContainer}>
@@ -60,9 +53,9 @@ export default function Home() {
                   </div>
                 </div>
                 <div className={styles.contentText}>
-                  <h2>Expert Roofing Services</h2>
+                <h2>Roofing Materials Expertise</h2>
                   <p>
-                    At Bro’s Construction, we specialize in high-quality roofing solutions tailored to your needs. Whether it’s a repair or a full replacement, our team delivers durability and craftsmanship.
+                  24 years of commercial and residential roofing, siding, and gutters. Bro’s Construction has installed and repaired it all; from flat roofing, TPO (Thermoplastic Polyolefin), EPDM (Ethylene Propylene Diene Monomer), and PVC, to metal roofing and traditional shingles or shake.
                   </p>
                 </div>
               </div>
@@ -73,7 +66,7 @@ export default function Home() {
                   <div className={styles.imageWrapper}>
                     <Image
                       src="/roofing-exterior-home.webp"
-                      alt="Siding Project"
+                      alt="Roofing Project, Park City Utah"
                       width={618}
                       height={363}
                       className={styles.contentImage}
@@ -81,9 +74,9 @@ export default function Home() {
                   </div>
                 </div>
                 <div className={styles.contentText}>
-                  <h2>Top-Notch Siding Installation</h2>
+                  <h2>Local Proficiency</h2>
                   <p>
-                    Enhance your home’s curb appeal and protection with our premium siding services. We offer a variety of materials and styles to match your vision and budget.
+                  Bro’s knows Utah! Rest assured that your project is in qualified, local hands. Heated roofing and snow retention roofs are a common task we perform throughout Salt Lake and Utah counties.
                   </p>
                 </div>
               </div>
@@ -102,9 +95,8 @@ export default function Home() {
                   </div>
                 </div>
                 <div className={styles.contentText}>
-                  <h2>Trusted Local Experts</h2>
-                  <p>
-                    Serving Salt Lake City and Utah County, we’re your local go-to for reliable construction services. Our reputation is built on trust, quality, and customer satisfaction.
+                <h2>Expert Roofing Services</h2>
+                  <p>Built to withstand Utah’s tough weather, from snowy winters to scorching summers, and the freezing and thawing in between.
                   </p>
                 </div>
               </div>

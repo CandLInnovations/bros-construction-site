0f0ce76ce1a6aeea4289ecc735d4d5b639cb3f95
Lisa Bria <lisabria@Lisas-MacBook-Air.local>
2025-10-09 16:45:43 -0700

    correction

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://brosconstruction.com'
+  const baseUrl = 'https://bros-construction.com'
 
   return [
     {
